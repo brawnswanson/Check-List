b7816af3819582035755344236c04d0517ff0ae4
Daniel Pressner <daniel.pressner@gmail.com>
2019-04-18 13:54:17 +0200

    Initial Commit

---
+++ Check List/ChecklistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Check List
+//
+//  Created by Daniel Pressner on 18.04.19.
+//  Copyright © 2019 Daniel Pressner. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
